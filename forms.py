d41f01b3fb795ee23d3e8b34c60f8350135d2a1e
Spencer Taira <spencermtaira@gmail.com>
2022-10-21 12:45:50 -0700

    added button with link to /add route on homepage, created pet_add_form html with form validation

--- forms.py
+++ forms.py
@@ -1,1 +1,23 @@
 """Forms for adopt app."""
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField
+from wtforms.validators import InputRequired, Optional, Email, URL
+
+
+class AddPetForm(FlaskForm):
+    """Form for adding snacks."""
+
+    name = StringField("Pet name", validators=[InputRequired()])
+    species = StringField("Species", validators=[InputRequired()])
+    photo_url = StringField("Photo URL", validators=[Optional(), URL()])
+    age = SelectField(
+        'Age',
+        choices=[
+            ('baby', 'Baby'),
+            ('young', 'Young'),
+            ('adult', 'Adult'),
+            ('senior', 'Senior')
+        ]
+    )
+    notes = TextAreaField('Notes', validators=[Optional()])
