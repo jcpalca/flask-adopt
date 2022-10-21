a57cc2853d5657871b0b000443cb6b9ad2f8466b
Joel Alcaraz <joelchristopheralcaraz@gmail.com>
2022-10-21 12:09:07 -0700

    Add root route

--- app.py
+++ app.py
@@ -1,16 +1,18 @@
 """Flask app for adopt app."""
 
-from flask import Flask
+from flask import Flask, render_template
 
 from flask_debugtoolbar import DebugToolbarExtension
 
-from models import db, connect_db
+from models import db, connect_db, Pet
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = "secret"
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///adopt"
+# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///adopt"
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+  "postgresql://otherjoel:hello@13.57.9.123/otherjoel")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 connect_db(app)
@@ -22,3 +24,10 @@ db.create_all()
 # app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
+
+@app.get("/")
+def list_pets():
+
+    pets = Pet.query.all()
+
+    return render_template("pet_list.html", pets=pets)
